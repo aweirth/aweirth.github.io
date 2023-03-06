3f01e2dc12af2836f8f61e013b6d6a0cdc99af31
Alex Weirth <123027477+aweirth@users.noreply.github.com>
2023-03-06 09:48:58 -0800

    Add files via upload

---
+++ aiaccess_script.js
@@ -0,0 +1,11 @@
+function toggler() {
+    const body = document.querySelector('body');
+    body.classList.toggle('dark-mode');
+    const header = document.querySelector('.header');
+    header.classList.toggle('dark-mode');
+  }
+
+
+
+
+
